46e680391a1f8dc9469178109b106c4818fd6ff6
BhargavSellam <147702067+BhargavSellam@users.noreply.github.com>
2023-11-08 15:33:00 +0530

    Update CreateEC2InstanceWithSSH-key.tf

--- CreateEC2InstanceWithSSH-key.tf
+++ CreateEC2InstanceWithSSH-key.tf
@@ -20,10 +20,6 @@ tag = {
  Name = "new-server"
 }
 }
-resource "aws_key_pair" "terraform_ec2_key" {
-	key_name = "terraform_ec2_key"
-	public_key = "${file("terraform_ec2_key.pub")}"
-}
 
 #### file ends here ####
 # terraform validate
